74098b8b073a39b258a21ed768ca850905678ef3
Tom Gray <tom.c.gray@gmail.com>
2023-04-29 17:25:41 +0100

    main

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from "./App";
 import { Maintenance } from "./Maintenance";
 import "./index.css";
 
-console.log(
-  "import.meta.env.VITE_IS_MAINTENANCE_MODE",
-  import.meta.env.VITE_IS_MAINTENANCE_MODE
-);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {import.meta.env.VITE_IS_MAINTENANCE_MODE === "true" && <Maintenance />}
